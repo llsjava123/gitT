4547026f3cd0e221d9116e532d721bc936f0f25c
lls <llsjava@126.com>
2023-03-13 13:06:43 +0800

    add t1 text

--- src/main/java/com/Class1.java
+++ src/main/java/com/Class1.java
@@ -5,6 +5,8 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("T1 ");
         System.out.println("T2 ");
+        System.out.println("T3  ");
+        System.out.println("T4  ");
     }
 
 }
